fe5abfc2fa5f5264c4f7752f52d11a01a842a710
anakon73 <nazarkinah73@gmail.com>
2022-12-06 18:49:25 +0200

    feat(homepage.vue): create event cards component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="text-center color-slate-700 font-sans p-2">
-    <nav class="p-[30px]">
-      <router-link
-        class="font-bold text-slate-700"
-        active-class="text-emerald-400"
-        to="/"
-        >Home</router-link
-      >
-      |
-      <router-link
-        class="font-bold text-slate-700"
-        active-class="text-emerald-400"
-        to="/about"
-        >About</router-link
-      >
-    </nav>
-    <router-view />
-  </div>
+  <nav class="p-[30px]">
+    <router-link
+      class="font-bold text-slate-700"
+      active-class="text-emerald-400"
+      to="/"
+      >Home</router-link
+    >
+    |
+    <router-link
+      class="font-bold text-slate-700"
+      active-class="text-emerald-400"
+      to="/about"
+      >About</router-link
+    >
+  </nav>
+  <router-view />
 </template>
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
-const event = {
-  id: 5928101,
-  category: 'animal welfare',
-  title: 'Cat Adoption Day',
-  description: 'Find your new feline friend at this event.',
-  location: 'Meow Town',
-  date: 'January 28, 2022',
-  time: '12:00',
-  petsAllowed: true,
-  organizer: 'Kat Laydee',
+interface Props {
+  event: {
+    id: string
+    category: string
+    title: string
+    description: string
+    location: string
+    date: string
+    time: string
+    petsAllowed: boolean
+    organizer: string
+  }
 }
+
+const { event } = defineProps<Props>()
 </script>
 
 <template>
@@ -17,8 +21,8 @@ const event = {
     class="p-5 w-[250px] cursor-pointer border border-slate-700 mb-[18px] hover:scale-[1.01] hover:shadow-[0_3px_12px_0_rgba(0,0,0,0.2)]"
   >
     <span>@ {{ event.time }} on {{ event.date }}</span>
-    <div class="text-base">
+    <h4 class="text-xl font-semibold">
       {{ event.title }}
-    </div>
+    </h4>
   </div>
 </template>
