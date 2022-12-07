1d5c1d853914a830c4130c7f6c19ab87f0262b76
anakon73 <nazarkinah73@gmail.com>
2022-12-07 16:55:53 +0200

    feat(eventlist): acceptance json file with events from outside

--- src/view/EventList.vue
+++ src/view/EventList.vue
@@ -1,4 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+type event = {
+  id: number
+  category: string
+  title: string
+  description: string
+  location: string
+  date: string
+  time: string
+  petsAllowed: boolean
+  organizer: string
+}
+
+const events = ref<event[]>([])
+
+axios
+  .get('https://my-json-server.typicode.com/anakon73/real-world-vue-3/events')
+  .then((response) => {
+    events.value = response.data
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+</script>
 
 <template>
   <h1 class="mb-[18px] text-[32px] font-bold">Events For Good</h1>
