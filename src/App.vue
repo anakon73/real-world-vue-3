2a2b16d0967612c1a3279866f23bf8a1c2f50ae0
anakon73 <nazarkinah73@gmail.com>
2022-12-08 15:11:51 +0200

    fix(index.html): corrections tailwind classes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav class="p-[30px]">
-    <router-link
-      class="font-bold text-slate-700"
-      active-class="text-emerald-400"
-      :to="{ name: 'EventsList' }"
-      >Events</router-link
-    >
-    |
-    <router-link
-      class="font-bold text-slate-700"
-      active-class="text-emerald-400"
-      :to="{ name: 'AboutPage' }"
-      >About</router-link
-    >
-  </nav>
-  <router-view />
+  <div class="text-center text-slate-700 font-sans m-2">
+    <nav class="p-[30px]">
+      <router-link
+        class="font-bold text-slate-700"
+        active-class="text-emerald-400"
+        :to="{ name: 'EventsList' }"
+        >Events</router-link
+      >
+      |
+      <router-link
+        class="font-bold text-slate-700"
+        active-class="text-emerald-400"
+        :to="{ name: 'AboutPage' }"
+        >About</router-link
+      >
+    </nav>
+    <router-view />
+  </div>
 </template>
